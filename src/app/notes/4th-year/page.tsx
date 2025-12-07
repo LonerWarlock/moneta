576ef3481dfb981ac19a942a26f87be7a4c293a0
Soham Sachin Phatak <sohamphatakssp@gmail.com>
2025-12-07 18:49:16 +0530

    SPM U4

--- src/app/notes/4th-year/page.tsx
+++ src/app/notes/4th-year/page.tsx
@@ -40,7 +40,7 @@ const fourthYearNotes = [
         {name: "Unit 1: Introduction to Software Project Management", link: "https://drive.google.com/file/d/11AsP7MkjSkzzzRH3Ex4W8DThlVx2YXIZ/view?usp=drive_link"},
         {name: "Unit 2: Project Design and Evaluation", link: "https://drive.google.com/file/d/1qMCYR6vt8G97q1mV0UKmxiA9hGj-OEck/view?usp=drive_link"},
         {name: "Unit 3: Activity Planning & Risk Management", link: "https://drive.google.com/file/d/10GKygkW0-9OPfFLtos7KOqA75vpwM6AF/view?usp=drive_link"},
-        //{name: "Unit 4: Project Tracking, Monitoring & Control", link: ""},
+        {name: "Unit 4: Project Tracking, Monitoring & Control", link: "https://drive.google.com/file/d/12O-GDHQ6x3vhbgNo0c4fI1kHil7xjpFI/view?usp=drive_link"},
         //{name: "Unit 5: Managing People and Organizing Teams", link: ""},
         {name: "SPM Endsem Decode", link: "https://drive.google.com/file/d/1cIAZhKBR0judIIUYSvVC_h29dRUE1nUG/view?usp=drive_link"},
         {name: "SPM Endsem Technical", link: "https://drive.google.com/file/d/16srza3muL9roptcsS8l7jO_G4-F7StMi/view?usp=drive_link"},
